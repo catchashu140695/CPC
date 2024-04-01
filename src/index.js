4d04b19fa9ad2c4f419649ec92f53d506bfaef69
catchashu140695 <101091750+catchashu140695@users.noreply.github.com>
2024-04-01 14:34:53 +0530

    some needed changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '../src/Assets/css/bootstrap.min.css'
+import '../src/Assets/css/style.css'
+import '../node_modules/jquery/dist/jquery.min.js'
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    
     <App />
   </React.StrictMode>
 );
