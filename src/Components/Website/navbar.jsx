99ffabbd6c21e5d171b1eb46651ace2b95fbb5ae
catchashu140695 <101091750+catchashu140695@users.noreply.github.com>
2024-04-01 15:19:56 +0530

    ok

--- src/Components/Website/navbar.jsx
+++ src/Components/Website/navbar.jsx
@@ -38,7 +38,7 @@ export default function Navbar() {
             </div>
 
             <nav className="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
-                <NavLink to="index.html" className="navbar-brand d-flex align-items-center border-end px-4 px-lg-5">
+                <NavLink to="/" className="navbar-brand d-flex align-items-center border-end px-4 px-lg-5">
                     <h2 className="m-0 text-primary">Cellera Power Care</h2>
                 </NavLink>
                 <button type="button" className="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
