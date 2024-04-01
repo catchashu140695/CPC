ca2d8d2f8759998c37e4d39448148daa4d12c9f9
catchashu140695 <101091750+catchashu140695@users.noreply.github.com>
2024-04-01 16:42:41 +0530

    ok

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import '../src/Assets/css/bootstrap.min.css'
-import '../src/Assets/css/style.css'
+
 import '../node_modules/jquery/dist/jquery.min.js'
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+
 
 
 
